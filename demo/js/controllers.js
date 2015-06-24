17efd704867a878caed21f996b628581b19dc752
rajeshwarpatlolla <rajeshwar.patlolla@gmail.com>
2015-06-24 16:35:00 +0530

    time changed for toasts

--- demo/js/controllers.js
+++ demo/js/controllers.js
@@ -3,15 +3,15 @@ angular.module('starter.controllers', [])
 .controller('HomeCtrl', function($scope, ionicToast) {
 
     $scope.showToastTop = function(){
-      ionicToast.show('This is a toast at the top.', 'top', true, 2500);
+      ionicToast.show('This is a toast at the top.', 'top', true, 2000);
     };
 
     $scope.showToastMiddle = function(){
-      ionicToast.show('This is a toast at the middle.', 'middle',false, 10000);
+      ionicToast.show('This is a toast at the middle.', 'middle',false, 1000);
     };
 
     $scope.showToastBottom = function(){
-      ionicToast.show('This is a toast at the bottom.', 'bottom',false, 20000);
+      ionicToast.show('This is a toast at the bottom.', 'bottom',false, 2000);
     };
 
   })
