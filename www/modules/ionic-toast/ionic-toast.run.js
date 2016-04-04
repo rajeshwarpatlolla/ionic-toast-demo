ec35e46813a55bc619015307f0cd458e57704161
rajeshwarpatlolla <rajeshwar.patlolla@gmail.com>
2016-04-04 13:17:49 +0530

    changes in css of the toast

--- www/modules/ionic-toast/ionic-toast.run.js
+++ www/modules/ionic-toast/ionic-toast.run.js
@@ -3,7 +3,7 @@ angular.module('ionic-toast')
 
   .run(['$templateCache', function ($templateCache) {
     var toastTemplate = '<div class="ionic_toast"><div class="toast_section" ng-class="ionicToast.toastClass" ng-style="ionicToast.toastStyle">' +
-      '<span class="ionic_toast_close" ng-click="hide()"><i class="ion-close-round toast_close_icon"></i></span>' +
+      '<span class="ionic_toast_close" ng-click="hide()"><i class="ion-android-close toast_close_icon"></i></span>' +
       '<span ng-bind-html="ionicToast.toastMessage"></span></div></div>';
 
     $templateCache.put('ionic-toast/templates/ionic-toast.html', toastTemplate);
